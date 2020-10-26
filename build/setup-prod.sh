96d0d9810d90696b207a9205226f3754b8b381a3
Natalja Balicka <natalja.balicka@gmail.com>
2020-10-26 12:14:22 +0200

    commit 6

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -5,4 +5,4 @@ echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile ./server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub 
